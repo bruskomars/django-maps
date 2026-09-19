c03909d1e9bad2d0a035d11a3d0ab8cfa16a5011
Mars Alexis Regidor <pythonista.mars@gmail.com>
2026-09-19 14:36:51 +0800

    added referer in map provider params to access osm

--- config/maps_frontend/static/main.js
+++ config/maps_frontend/static/main.js
@@ -3,13 +3,11 @@ document.addEventListener("DOMContentLoaded", init);
 function init() {
   // Leaflet map initialization
   const map = L.map("map").setView([14.516589, 121.019333], 13);
-  L.tileLayer(
-    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
-    {
-      attribution:
-        "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012",
-    },
-  ).addTo(map);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    referrerPolicy: "no-referrer-when-downgrade",
+    attribution: "&copy; OpenStreetMap contributors",
+  }).addTo(map);
 
   // fetch data from the backend API
   const fetchGetRequest = async (url, func) => {
